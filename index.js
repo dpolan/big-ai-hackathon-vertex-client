7b7c564fcea43a84cd54cf7b409879fd49071b2c
David González Polán <dpolan@oldboycd.com>
2023-08-20 13:15:19 +0200

    Update alternative port

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post("/collector", jsonParser, async function (req, res, next) {
   res.send(JSON.stringify(prediction));
 });
 
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 7777, function () {
   var host = server.address().address;
   var port = server.address().port;
 
