14a4c48d81359b7aeba52b68ef3dd0e8cdd008ba
David González Polán <dpolan@oldboycd.com>
2023-08-20 13:27:07 +0200

    Add OPTIONS request handler

--- index.js
+++ index.js
@@ -68,6 +68,19 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.options("/*", function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "bigcommerce-x-google-bigai-hackathon.mybigcommerce.com"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  res.send(200);
+});
+
 app.get("/", function (req, res, next) {
   res.send("Permission denied.");
 });
