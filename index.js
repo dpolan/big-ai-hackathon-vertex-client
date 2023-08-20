9c5bceebf5c80b99b1079c92a166f84aedcd8d74
David González Polán <dpolan@oldboycd.com>
2023-08-20 13:29:36 +0200

    Update Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use(function (req, res, next) {
 app.options("/*", function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "bigcommerce-x-google-bigai-hackathon.mybigcommerce.com"
+    "https://bigcommerce-x-google-bigai-hackathon.mybigcommerce.com"
   );
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
